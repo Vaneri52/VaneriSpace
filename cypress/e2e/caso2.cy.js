caf47d1e684a2a3beec9dc84380b06bbde82dd8c
Vaneri52 <vaneri5200@gmail.com>
2023-03-13 19:36:49 -0500

    Modificación caso2

--- cypress/e2e/caso2.cy.js
+++ cypress/e2e/caso2.cy.js
@@ -4,8 +4,8 @@ describe('Search test cases', function()
   {
     cy.visit('https://www.wingo.com/')
 
-    cy.get('.input-origen').should('have.value', 'Bogotá (BOG) El Dorado')
-    cy.get('select').click({force: true})
+    cy.get('.input-origen')
+    cy.get('select').should('have.value', 'Bogotá (BOG) El Dorado').click({force: true})
 
     cy.get('.input-destino').click({force: true})
     cy.get('select').should('have.value', 'Cali (CLO) Alfonso Bonilla Aragón')
